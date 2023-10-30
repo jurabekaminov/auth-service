980270bcb09d82a85add828c4c7555ef6ed4646b
1ordhokage <dzhurabek.aminov@mail.ru>
2023-10-30 20:34:19 +0300

    ✅ added jwt schemas

---
+++ src/schemas/token.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str
+    
+    
+class TokenPayloadSchema(BaseModel):
+    iat: datetime
+    exp: datetime
+    sub: str
+    role: str
+    email: str

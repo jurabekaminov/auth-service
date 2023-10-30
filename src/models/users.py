ff64f9d4196104027f0690624e74239ea4f0cade
1ordhokage <dzhurabek.aminov@mail.ru>
2023-10-30 20:33:43 +0300

    🔄 changed user model

--- src/models/users.py
+++ src/models/users.py
@@ -9,6 +9,5 @@ class User(Base):
     
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
-    password_hash: Mapped[str] = mapped_column(String(128), nullable=False)
+    password_hashed: Mapped[str] = mapped_column(String(256), nullable=False)
     role: Mapped[str] = mapped_column(String(64), nullable=False)

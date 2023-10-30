1f53ef23a7dd42e8a82575fd3364c0847b4424a2
Jurabek Aminov <dzhurabek.aminov@gmail.com>
2023-10-30 20:34:38 +0300

    ✅ added user schemas

---
+++ src/schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+
+from src.utils.roles import RoleEnum
+
+
+class UserUpdateSchema(BaseModel):
+    email: str = Field(max_length=128)
+    text_password: str = Field(max_length=64)
+
+
+class UserCreateSchema(UserUpdateSchema):
+    role: RoleEnum
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    email: str = Field(max_length=128)
+    role: RoleEnum

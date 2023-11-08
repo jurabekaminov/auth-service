dd8c205c30c0587be09aa3f31dc12ee458041ee4
1ordhokage <dzhurabek.aminov@mail.ru>
2023-11-08 22:37:50 +0300

    ✅ added created_by field

--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
 
@@ -11,3 +11,4 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(128), unique=True, nullable=False)
     password_hashed: Mapped[str] = mapped_column(String(256), nullable=False)
     role: Mapped[str] = mapped_column(String(64), nullable=False)
+    created_by: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
